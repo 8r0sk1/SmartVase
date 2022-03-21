2e5bbcfeddb0587ad088c19b42c91a9ed7d7662d
vitori98 <s287686@studenti.polito.it>
2022-03-21 17:28:13 +0100

    cazzini firebase e viewmodel

--- app/src/main/java/it/polito/did/smartvase/ui/main/MainViewModel.kt
+++ app/src/main/java/it/polito/did/smartvase/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package it.polito.did.smartvase.ui.main
 
+import android.widget.ImageView
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
@@ -7,4 +8,6 @@ class MainViewModel : ViewModel() {
     public var notification : Boolean = true
     public var waterLevel : Float = 0
     public var soilMoisture : Float = 0
+    public lateinit var plantIcon : ImageView  //nomi file immagini profilo
+    public var plantName : String = "Ficus"
 }
