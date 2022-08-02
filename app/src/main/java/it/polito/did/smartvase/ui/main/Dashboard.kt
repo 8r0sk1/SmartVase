f07fabd5da647a8ea847f92200e104a860d7d0a6
vitori98 <s287686@studenti.polito.it>
2022-08-02 12:38:11 +0200

    sto a sgrava

--- app/src/main/java/it/polito/did/smartvase/ui/main/Dashboard.kt
+++ app/src/main/java/it/polito/did/smartvase/ui/main/Dashboard.kt
@@ -3,15 +3,16 @@ package it.polito.did. smartvase.ui.main
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Vibrator
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.marginTop
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.transition.TransitionInflater
@@ -19,6 +20,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import it.polito.did.smartvase.R
+import it.polito.did.smartvase.ui.main.Homepage
+import it.polito.did.smartvase.ui.main.MainViewModel
 
 class Dashboard : Fragment() {
 
@@ -34,47 +37,6 @@ class Dashboard : Fragment() {
         enterTransition = inflater.inflateTransition(R.transition.slide)
     }
 
-    /*override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val plantIcon = view?.findViewById<TextView>(R.id.PlantIcon)
-
-        val waterBar = view?.findViewById<ImageView>(R.id.waterBarFull)
-        val soilMostureBar = view?.findViewById<ImageView>(R.id.soilMoisrtureBarFull)
-        val plantName = view?.findViewById<TextView>(R.id.PlantName2)
-        val plantIcon = view?.findViewById<TextView>(R.id.PlantIcon)
-        val offText = view?.findViewById<TextView>(R.id.offText)
-        val waterButton = view?.findViewById<Button>(R.id.waterButton)
-        val notificationButton = view?.findViewById<Button>(R.id.notificationButton)
-
-
-        if (plantName != null) {
-            plantName.text=viewModel.plantName
-        }
-        //TODO METTERE IMMAGINE PIANTA plantIcon=viewModel.plantIcon
-        //waterBar.height=viewModel.waterLevel
-        //soilMostureBar.height=viewModel.soilMoisture
-
-        if(viewModel.auto) {
-            if (offText != null) {
-                offText.setText("OFF")
-            }
-        }
-        else {
-            //TODO TASTO WATER IN DENTRO
-            if (offText != null) {
-                offText.setText("ON")
-            }
-        }
-        //if(viewModel.notification)
-        //TODO STATE ICONA NOTIFICHE
-        //todo peppinodicapri
-        viewModel.auto=!viewModel.auto
-
-        return inflater.inflate(R.layout.dashboard, container, false)
-    }*/
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -85,46 +47,72 @@ class Dashboard : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var waterBar = view?.findViewById<CardView>(R.id.waterLevelBar)
-        var soilMostureBar = view?.findViewById<CardView>(R.id.soilMoistureBar)
-        val plantName = view?.findViewById<TextView>(R.id.PlantName)
-        val plantIcon = view?.findViewById<ImageView>(R.id.PlantIcon)
-        val offText = view?.findViewById<TextView>(R.id.offText)
-        val homeButton = view.findViewById<ImageButton>(R.id.homeButton)
-        val editButton = view.findViewById<ImageButton>(R.id.editButton)
-        val waterButton = view.findViewById<FloatingActionButton>(R.id.autoWaterButton)
-        val notificationButton = view.findViewById<ImageButton>(R.id.notificationButton)
+        var waterBar = view?.findViewById<CardView>(R.id.waterLevelBar2)
+        var soilMostureBar = view?.findViewById<CardView>(R.id.soilMoistureBar2)
+        val plantName = view?.findViewById<TextView>(R.id.plantName2)
+        val plantIcon = view?.findViewById<ImageView>(R.id.PlantIcon2)
+        val offText = view?.findViewById<TextView>(R.id.offText2)
+        val homeButton = view.findViewById<ImageButton>(R.id.homeButton2)
+        val editButton = view.findViewById<ImageButton>(R.id.editButton2)
+        val autoWaterButton = view.findViewById<FloatingActionButton>(R.id.autoWaterButton2)
+        val notificationButton = view.findViewById<ImageButton>(R.id.notificationButton2)
+        val SoilMoisturePercentage = view?.findViewById<TextView>(R.id.SoilMoisturePercentage)
+        val WaterLevelPercentage = view?.findViewById<TextView>(R.id.WaterLevelPercentage)
+        val dividerMaxWaterLevel = view?.findViewById<ImageView>(R.id.dividerMaxWaterLevel2)
+        val dividerMinWaterLevel = view?.findViewById<ImageView>(R.id.dividerMinWaterLevel2)
+        val dividerMaxSoilMoisture = view?.findViewById<ImageView>(R.id.dividerMaxSoilMoisture2)
+        val dividerMinSoilMoisture = view?.findViewById<ImageView>(R.id.dividerMinSoilMoisture2)
 
-        val db = Firebase.database.reference
+        val db = Firebase.database.reference //TODO VLAD TE TENGO DOCCHIOOOO
         val ref = db.child("A7/toWaterControl")
 
-        //popolamento viste
-        var fillWater :Float=385-(3.85f * viewModel.waterLevel)
-        var fillSoil :Float =385-(3.85f * viewModel.soilMoisture)
-        waterBar?.marginTop ?: fillWater
-        soilMostureBar?.marginTop ?: fillSoil
+
+                                        //popolamento viste
+
+        viewModel.plantIconId= R.mipmap.ic_launcher
+        plantName?.setText(viewModel.plantName)
+        plantIcon?.setImageResource(viewModel.plantIconId) //TODO COMe si assegnano le immagini porcapputttana?
+
+        //testo percentuale riempimento
+        SoilMoisturePercentage?.text= viewModel.waterLevel.toString()
+        WaterLevelPercentage?.text= viewModel.soilMoisture.toString()
+
+        //divisori barre
+        dividerMaxWaterLevel?.updateLayoutParams<ConstraintLayout.LayoutParams> { verticalBias = 1-0.01f*viewModel.waterLevelMax  }
+        dividerMinWaterLevel?.updateLayoutParams<ConstraintLayout.LayoutParams> { verticalBias = 1-0.01f*viewModel.waterLevelMin  }
+        dividerMaxSoilMoisture?.updateLayoutParams<ConstraintLayout.LayoutParams> { verticalBias = 1-0.01f*viewModel.soilMoistureMax  }
+        dividerMinSoilMoisture?.updateLayoutParams<ConstraintLayout.LayoutParams> { verticalBias = 1-0.01f*viewModel.soilMoistureMin  }
+
+        //da percentuale a valore di traslazione (385dp lunghezza barra)
+        var fillWater :Float=385- 3.85f * viewModel.waterLevel //TODO DA DP A PIXEL DI MERDA
+        var fillSoil :Float =385- 3.85f * viewModel.soilMoisture
+
+
+        //riempire barre
+        waterBar.translationY =  * 385f *0.5f
+        soilMostureBar.translationY = 154f
+
         if(viewModel.auto){
             offText?.text="ON"
             offText?.setTextColor(Color.RED)
         }
 
-
-        waterButton.setOnClickListener {
-            ref.setValue(1)
-        }
-
-
         homeButton.setOnClickListener { findNavController().navigate(R.id.action_dashboard_to_homepage) }
         editButton.setOnClickListener { findNavController().navigate(R.id.action_dashboard_to_editPlant) } //ancora da capire come fare, se con altro fragment
 
-        waterButton.setOnLongClickListener{
-            viewModel.auto=true
+        autoWaterButton.setOnLongClickListener{
+            viewModel.auto = !viewModel.auto
+            if(viewModel.auto)
+                offText?.text="ON"
+            else
+                offText?.text="OFF"
+            ref.setValue(1) //TODO VLADDO
             // activity?.vibrate(25);
             return@setOnLongClickListener true
         }
 
-        notificationButton.setOnClickListener{
-            viewModel.notification=false
+        notificationButton.setOnClickListener {
+            viewModel.notification = !viewModel.notification
         }
     }
 
--- app/src/main/java/it/polito/did/smartvase/ui/main/MainViewModel.kt
+++ app/src/main/java/it/polito/did/smartvase/ui/main/MainViewModel.kt
@@ -1,13 +1,27 @@
 package it.polito.did.smartvase.ui.main
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModel
+import it.polito.did.smartvase.R
 
 class MainViewModel : ViewModel() {
     public var auto : Boolean = false
     public var notification : Boolean = true
-    public var waterLevel : Float = 0f //in percentuale per come fatto su dashboard
-    public var soilMoisture : Float = 0f
-    public lateinit var plantIcon : ImageView //nomi file immagini profilo
-    public var plantName : String = "Ficus"
+
+    //in percentuale per come fatto su dashboard
+    public var waterLevel : Float = 30f
+    public var waterLevelMax : Float = 90f
+    public var waterLevelMin : Float = 25f
+    public var soilMoisture : Float = 60f
+    public var soilMoistureMax : Float = 80f
+    public var soilMoistureMin : Float = 25f
+
+    public var plantName : String = "Ficus69xxx"
+    public lateinit var plantIcon : Drawable //nomi file immagini profilo
+    public var plantIconId : Int = 0 //nomi file immagini profilo
 }
